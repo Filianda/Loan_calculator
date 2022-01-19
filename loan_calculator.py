bad5fe6195cf404d0c84ef78def20f2f38cea305
Filianda <filianda99@gmail.com>
2022-01-20 00:01:03 +0100

    stage 2

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,6 +1,38 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
-print(f'{loan_principal}\n{first_month}\n{second_month}\n{third_month}\n{final_output}')
+import math
+def user_inputs():
+    monthly_payment = 0
+    months_number = 0
+    loan_principal = int(input('Loan principal: '))
+    payments_category = input('What do you want to calculate?\ntype "m" - for number of monthly payments,\ntype "p" - for the monthly payment:\n')
+    if payments_category == 'm':
+        monthly_payment = int(input('Enter the monthly payment:\n'))
+    elif payments_category == 'p':
+        months_number = int(input('Enter the number of months:\n'))
+    return [monthly_payment,months_number,loan_principal, payments_category]
+
+def calculator(monthly_payment,months_number,loan_principal, payments_category):
+    if payments_category == 'm':
+        months_number = math.ceil(loan_principal/monthly_payment)
+        if months_number == 1:
+            month_form = 'month'
+        else:
+            month_form = 'months'
+        print(f'\nIt will take {months_number} {month_form} to repay the loan')
+    else:
+        monthly_payment = math.ceil(loan_principal/months_number)
+        last_payment = loan_principal - (months_number - 1) * monthly_payment
+        if monthly_payment == last_payment:
+            print(f'\nYour monthly payment = {monthly_payment}')
+        else:
+            print(f'\nYour monthly payment = {monthly_payment} and the last payment = {last_payment}')
+    return [months_number,monthly_payment]
+
+def main():
+    user_paramiters = user_inputs()
+    monthly_payment = user_paramiters[0]
+    months_number = user_paramiters[1]
+    loan_principal = user_paramiters[2]
+    payments_category = user_paramiters[3]
+    calculator(monthly_payment,months_number,loan_principal, payments_category)
+
+main()
